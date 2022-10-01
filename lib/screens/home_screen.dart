c07bcafba9ce558b86eb9d228b3793d2b595f6cb
Tarun-314 <6213111566f@gmail.com>
2022-10-01 17:11:23 +0530

    Student list app refer this for seach

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:studentform/screens/login._page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:studentform/screens/view_page.dart';
+import 'package:studentform/screens/edit.dart';
+import 'dart:io';
+class home extends StatefulWidget {
+  home({Key? key}) : super(key: key);
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  String srch="";
+  final img='assets/prof.jpg';
+  final bak='assets/load1.png';
+  TextEditingController search=TextEditingController();
+  ScrollController bar=ScrollController();
+  List<String> list=["Edit","Delete"];
+  @override
+  Widget build(BuildContext context) {
+
+    return  Scaffold(
+
+      appBar: AppBar(
+        centerTitle: true,
+        shadowColor: Colors.black,
+        backgroundColor: Colors.black87,
+        leading: const Padding(
+          padding: EdgeInsets.fromLTRB(80, 0, 0, 0),
+         child:Icon(Icons.person),
+        ),
+        title: const Text("Student Profile List"),
+        titleTextStyle: const TextStyle(fontWeight: FontWeight.w900,fontSize: 20.0),
+      ),
+      body:Container(
+        decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.cover,image: AssetImage(bak))),
+      child:RawScrollbar(
+
+         thickness: 40,
+        thumbVisibility: true,
+        timeToFade: Duration(seconds: 6),
+        interactive: true,
+        fadeDuration: Duration(seconds: 6),
+        radius: Radius.circular(30),
+
+
+        child:SingleChildScrollView(
+          child:Padding(
+           padding: const EdgeInsets.only(right: 20,left: 10),
+           child: Column(
+            children: [
+              SizedBox(height: 20,),
+              Container(
+                height: 50,
+              decoration: BoxDecoration(color: Colors.white,borderRadius:BorderRadius.circular(20.0) ),
+              child:TextField(
+                controller: search,
+                onChanged: (val) => {
+                      setState((){
+                        srch=val;
+                      })
+                },
+                cursorColor: Colors.black87,
+                decoration:InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical:10.0 ,horizontal:10.0 ),
+                  hintText: "Search",
+
+
+                  suffixIcon: Icon(Icons.search_sharp),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                ),
+              ),
+              ),
+              SizedBox(height: 35,),
+
+              StreamBuilder<QuerySnapshot>(stream: FirebaseFirestore.instance.collection("Login").snapshots(),
+                  builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
+                    if(snapshot.connectionState == ConnectionState.waiting){
+                      return const Center(child: CircularProgressIndicator(),
+                      );
+                    }
+                    if(snapshot.hasData){
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          var data=snapshot.data!.docs[index].data() as Map<String,dynamic>;
+                          if(srch.isEmpty){
+                            return studentCard(data,(){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=> viewpage(data)));});
+                          }
+                          if(data['name'].toString().toLowerCase().contains(srch.toLowerCase())||
+                              data['roll_no'].toString().toLowerCase().contains(srch.toLowerCase())){
+                            return studentCard(data,(){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> viewpage(data)));});
+                          }
+                          return ColoredBox(color: Colors.white);
+
+                        },
+                        // shrinkWrap: true,
+                        // children: snapshot.data!.docs.map((stid) => studentCard(stid,(){
+                        //   Navigator.push(context, MaterialPageRoute(builder: (context)=> viewpage(stid)));
+                        //   })).toList(),
+
+                      );
+                    }
+                    return Text("No Details to View", style: GoogleFonts.nunito(fontSize: 17,color:Colors.black ),);
+                  },
+                ),
+
+
+            ],
+          ),
+
+      ),
+      ),
+      ),
+      ),
+
+     //backgroundColor: Colors.black,
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text("Add Profile"),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder:(context) => login(),));
+        },
+        icon: Icon(Icons.person_add),
+
+      ),
+    );
+  }
+
+ Widget studentCard(Map<String,dynamic> doct,Function()? onDoubleTap) {
+
+        return GestureDetector(
+          onLongPress: (){},
+          onDoubleTap: onDoubleTap,
+          child:Column(
+          children:[
+            Container(
+
+            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10.0)),
+            child:
+             ListTile(
+
+                 // decoration: BoxDecoration(
+             //   color: Colors.black87,
+             //
+             //   borderRadius: BorderRadius.circular(8.0),
+             // ) ,
+              tileColor: Colors.teal,
+              onTap: (){},
+              selected: true,
+
+              title: Text(
+                doct['roll_no'],
+                style:GoogleFonts.notoSansHanunoo(color: Colors.black,fontWeight:FontWeight.bold),
+                overflow: TextOverflow.fade,
+              ),
+              subtitle: Text(
+                doct['name'],
+                style:GoogleFonts.notoSansHanunoo(color: Colors.black),
+                overflow: TextOverflow.fade,
+              ),
+          // decoration: UnderlineTabIndicator( insets: EdgeInsets.all(2.0)),
+             leading:Container(
+               width: 90,
+               height: 90,
+               child:Row(
+                children: [
+                  if(doct['ipath']=="")CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage(img),
+                  ),
+                  if(doct['ipath']!="")CircleAvatar(
+                    radius: 30,
+                    backgroundImage: FileImage(File(doct['ipath'])),
+                  ),
+              ],
+             ),
+             ),
+
+                trailing:PopupMenuButton(itemBuilder: (context) {
+                    return list.map((e) => PopupMenuItem(
+                      value: e,
+                      child: Row(
+                        children: [
+                          if(e=="Edit")Icon(Icons.edit,size: 30,color: Colors.black,),
+
+                       if(e=="Delete")Icon(Icons.delete,size: 30,color: Colors.black,),
+                          SizedBox(width: 20,),
+                          Text(e,style: GoogleFonts.archivoBlack(fontWeight: FontWeight.w400,color: Colors.black),)
+                        ],
+                      ),
+                    )).toList();
+
+                  },
+                    onSelected: (value){
+                      if(value=="Edit"){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>editer(doct)));
+                      }
+                      else{
+                        FirebaseFirestore.instance.collection("Login").doc(doct['docid']).delete();
+
+                      }
+                    },
+                    icon: Icon(Icons.more_vert_sharp),
+                    color: Colors.white,
+                  ),
+                )
+               ),
+
+             SizedBox(height: 13,)
+            ],
+          ),
+        );
+
+  }
+
+}
+
---
+++ lib/screens/login._page.dart
@@ -0,0 +1,368 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:io';
+import 'package:studentform/screens/home_screen.dart';
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  String path="";
+  TextEditingController name=TextEditingController();
+  TextEditingController email=TextEditingController();
+  TextEditingController rollno=TextEditingController();
+  GlobalKey<FormState> formKey= GlobalKey<FormState>();
+  final img='assets/prof.jpg';
+  final log1='assets/log1.jpg';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          backgroundColor: Colors.white,
+
+          appBar: AppBar(
+            backgroundColor: Colors.black87,
+            titleSpacing: 25.0,
+          title:Container(
+
+            child: Row(
+
+              children: const [
+
+                Icon(Icons.person_add),
+                SizedBox(width: 10,),
+                Text("Student Login Details")
+              ],
+            ),
+          ),
+            titleTextStyle: const TextStyle(fontWeight: FontWeight.w900,fontSize: 20.0),
+
+
+      ),
+          body:Container(
+            decoration: BoxDecoration(image: DecorationImage( alignment: Alignment.center,fit: BoxFit.cover,image: AssetImage(log1))),
+          child:SingleChildScrollView(
+
+          child:Padding(
+
+          padding: const EdgeInsets.all(10.0),
+          child:Form(
+            key: formKey,
+            child:Column(
+            crossAxisAlignment:CrossAxisAlignment.start ,
+            children: [
+              const SizedBox(height: 30,),
+            Center(
+              child : Stack(
+
+                  children:[
+                    if(path=="")CircleAvatar(
+                      radius: 120,
+                      backgroundImage: AssetImage(img) ,),
+                    if(path!="")CircleAvatar(
+                      radius: 120,
+                      backgroundImage: FileImage(File(path),),
+
+                      ),
+
+
+                    Positioned(
+                      bottom: 20,
+                      right: 20,
+
+                      child:CircleAvatar(
+                        backgroundColor: Colors.blue,
+                          radius: 20,
+                          child:IconButton(
+                            icon: Icon(Icons.camera_enhance,color: Colors.tealAccent),
+                            color: Colors.white,
+
+                            visualDensity: VisualDensity.adaptivePlatformDensity,
+                            onPressed: () {
+                              showModalBottomSheet(
+                                context: context,
+                                builder: ((builder)=>bottomsheet()) ,
+                              );
+                            },
+
+                          )
+
+                      ),
+                    ),
+                  ]
+              ),
+
+            ),
+            const SizedBox(height: 40,),
+            Center(
+              child:Container(
+                width: 308,
+                decoration: UnderlineTabIndicator(borderSide: BorderSide(width: 2.0) ),
+              child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child:SizedBox(
+                    width:260,
+                    child:TextFormField(
+                      textAlign: TextAlign.start,
+                      controller: name,
+                      decoration: const InputDecoration(
+                          hintText: 'Name',
+                          prefixIcon: Icon(Icons.perm_identity),
+                          border: InputBorder.none
+                          // border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)))
+
+                      ),
+                      validator: (value) {
+                        if(value!.isEmpty||!RegExp(r'^[a-z A-z]+$').hasMatch(value!)){
+                          return "Enter a valid Name";
+                        }
+                        else{
+                          return null;
+                        }
+                      },
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: (){
+                  setState(() {
+                    name=TextEditingController(text: "");
+                  });
+                },
+                    icon: Icon(Icons.clear_sharp))
+              ],
+            ),
+            ),
+            ),
+            const SizedBox(height: 20,),
+            Center(
+              child:Container(
+              width: 308,
+              decoration: UnderlineTabIndicator(borderSide: BorderSide(width: 2.0) ),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child:SizedBox(
+                      width:260,
+                      child:TextFormField(
+                        textAlign: TextAlign.start,
+                        controller: email,
+                        decoration: const InputDecoration(
+                            hintText: 'Email',
+                            prefixIcon: Icon(Icons.alternate_email_outlined),
+                            border: InputBorder.none
+                            // border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)))
+
+                        ),
+                        validator: (value) {
+                          if(value!.isEmpty||!RegExp(r'^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$').hasMatch(value!)){
+                            return "Enter a valid Email";
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: (){
+                    setState(() {
+                      email=TextEditingController(text: "");
+                    });
+                  },
+                      icon: Icon(Icons.clear_sharp))
+                ],
+              ),
+            ),
+            ),
+            SizedBox(height: 20,),
+            Center(
+              child:Container(
+                width: 308,
+                decoration: UnderlineTabIndicator(borderSide: BorderSide(width: 2.0) ),
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child:Container(
+                      width:260 ,
+
+                      child:TextFormField(
+                        textAlign: TextAlign.start,
+                        controller: rollno,
+                        decoration: const InputDecoration(
+                            hintText: 'Roll Number',
+                            prefixIcon: Icon(Icons.numbers_outlined),
+                          border: InputBorder.none,
+                        ),
+                        validator: (value) {
+                          if(value!.isEmpty||!RegExp(r'^[a-z 0-9 A-z]{10}$').hasMatch(value!)){
+                            return "Enter a valid Roll Number";
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: (){
+                    setState(() {
+                      rollno=TextEditingController(text: "");
+                    });
+                  },
+                      icon: Icon(Icons.clear_sharp))
+                ],
+              ),
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+
+              FloatingActionButton.extended(
+                  label: const Text("clear"),
+                  backgroundColor: Colors.black,
+                  elevation: 10.0,
+                  extendedPadding: EdgeInsets.all(40.0),
+                  onPressed: (){
+                    setState(() {
+                      name=TextEditingController(text: "");
+                      email=TextEditingController(text: "");
+                      rollno=TextEditingController(text: "");
+                    });
+                  },
+              ),
+
+              const SizedBox(width: 73,),
+              FloatingActionButton.extended(
+                  label: Text("Submit"),
+                    backgroundColor: Colors.black,
+                    elevation: 10.0,
+                    extendedPadding: EdgeInsets.all(40.0),
+                    onPressed: ()async {
+
+                      FirebaseFirestore.instance.collection("Login").add({
+                        "name":name.text,
+                        "email":email.text,
+                        "roll_no":rollno.text,
+                        "ipath":path,
+                        "docid":"",
+                      }).then((value){
+                          FirebaseFirestore.instance.collection("Login").doc(value.id).update(
+                              {"docid":value.id});
+                        Navigator.pop(context);
+                      }).catchError((error) => print("Failed to add new profile due to $error"));
+                    } ,
+
+               ),
+
+
+           ]
+            ),
+              SizedBox(height: 400,)
+          ],
+        ),
+      ),
+          ),
+          ),
+          ),
+    );
+  }
+  Widget bottomsheet() {
+    return Container(
+      height: 125.0,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white70,
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+          if(path=="")FloatingActionButton.extended(
+              label: Text("Camera"),
+              onPressed:()async{
+                final pick= await ImagePicker().pickImage(source:ImageSource.camera);
+                if(pick==null){
+                  Navigator.pop(context);
+                  return;
+                }
+                else{
+                  setState((){
+                    path=pick.path;
+                  });
+                }
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.camera),
+              backgroundColor: Colors.black87
+          ),
+          if(path=="")SizedBox(width: 69.0,),
+          if(path=="")FloatingActionButton.extended(
+              label: Text("Gallery"),
+              onPressed: ()async{
+                final pick= await ImagePicker().pickImage(source:ImageSource.gallery);
+                if(pick==null){
+                  Navigator.pop(context);
+                  return;
+                }
+                else{
+                  setState((){
+                    path=pick.path;
+                  });
+                }
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.image),
+              backgroundColor: Colors.black87
+          ),
+          if(path!="")FloatingActionButton.extended(
+              label: Text("delete"),
+              onPressed:(){
+                setState((){
+                  path="";
+                });
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.delete),
+              backgroundColor: Colors.black87
+          ),
+          if(path!="")SizedBox(width: 69.0,),
+          if(path!="")FloatingActionButton.extended(
+              label: Text("Replace"),
+              onPressed: ()async{
+                final pick= await ImagePicker().pickImage(source:ImageSource.gallery);
+                if(pick==null){
+                  Navigator.pop(context);
+                  return;
+                }
+                else{
+                  setState((){
+                    path=pick.path;
+                  });
+                }
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.image_outlined),
+              backgroundColor: Colors.black87
+          ),
+
+
+
+        ],
+      ),
+    );
+  }
+}
